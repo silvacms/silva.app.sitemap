94fa4eebcb1299df907ecd51fee2fd9eb0086e25
Antonin Amand <antonin.amand@gmail.com>
2012-09-17 15:01:12 +0200

    init package

---
+++ README.txt
@@ -0,0 +1,15 @@
+=================
+silva.app.sitemap
+=================
+
+Introduction
+============
+
+``silva.app.sitemap`` contains views and local configuration service for
+xml sitemaps 0.9 (see http://www.sitemaps.org/protocol.html).
+
+Code repository
+===============
+
+Source code can be found at :
+https://hg.infrae.com/silva.app.sitemap
---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2002-2011 Infrae. All rights reserved.
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+import os
+
+version = '3.0dev'
+
+tests_require = [
+    'Products.Silva [test]',
+    ]
+
+setup(name='silva.app.sitemap',
+      version=version,
+      description="Sitemap xml for silva",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silva sitemap',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='http://infrae.com/products/silva',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['silva', 'silva.core'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'Zope2',
+        'five.grok',
+        'grokcore.chameleon',
+        'setuptools',
+        ],
+      tests_require=tests_require,
+      extras_require={'test': tests_require},
+      )
