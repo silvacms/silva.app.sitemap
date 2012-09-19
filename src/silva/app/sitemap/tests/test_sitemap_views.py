021d2580f19fb2816954982e3b4adfb405f96b35
Antonin Amand <antonin.amand@gmail.com>
2012-09-19 19:06:58 +0200

    tests

--- src/silva/app/sitemap/tests/test_sitemap_views.py
+++ src/silva/app/sitemap/tests/test_sitemap_views.py
@@ -3,7 +3,10 @@
 import unittest
 
 from zope.component import queryUtility
+from zope.event import notify
+from zope.lifecycleevent import ObjectModifiedEvent
 
+from silva.core.interfaces import IPublicationWorkflow
 from ..interfaces import ISitemapService
 from ..testing import FunctionalLayer
 
@@ -13,19 +16,23 @@ class SitemapViewTestCase(unittest.TestCase):
 
     def setUp(self):
         self.root = self.layer.get_application()
+        self.layer.login('manager')
         factory = self.root.manage_addProduct['Silva']
         factory.manage_addFolder('folder', 'Folder')
         factory = self.root.folder.manage_addProduct['Silva']
-        with self.layer.open_fixture('photo.tif') as image:
-            factory.manage_addImage('test_image', u'Image élaboré', image)
+        factory.manage_addMockupVersionedContent('index', 'Index')
+        notify(ObjectModifiedEvent(self.root.folder))
+        IPublicationWorkflow(self.root.folder.index).publish()
+        self.assertTrue(self.root.folder.is_published())
 
     def test_simple_view(self):
         service = queryUtility(ISitemapService)
-        service.set_allowed_meta_types(['Silva Root', 'Silva Folder', 'Silva Image'])
+        service.set_trim_index(False)
+        service.set_allowed_meta_types([
+            'Mockup VersionedContent'])
 
         with self.layer.get_browser() as browser:
             self.assertEquals(browser.open('/root/sitemap.xml'), 200)
-            self.assertEquals("", browser.contents)
 
 
 def test_suite():
